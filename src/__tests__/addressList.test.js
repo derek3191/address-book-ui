e62f616264c7768611162cb78e06ea83d3144116
dmoring <derek3191+xcodegit@me.com>
2020-10-24 18:37:49 -0500

    updated addresslist tests

--- src/__tests__/addressList.test.js
+++ src/__tests__/addressList.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { fireEvent, cleanup } from '@testing-library/react';
 import AddressList from '../components/addressList';
-
+import address from '../services/address';
 
 require('dotenv').config();
 //import renderer from 'react-test-renderer';
@@ -12,9 +12,30 @@ describe('<AddressList />', () => {
 
     describe('get addresses', () => {
         it('should return a list of addresses', () => {
-            let addresses = AddressList.prototype.getAllAddresses();
-            expect(addresses).not.toBeUndefined();
+            let addresses = [];
+            address.prototype.getAllAddresses()
+                .then(res => {
+                    addresses = res.data
+                })
+                .then(() => {
+                    expect(addresses).not.toBeUndefined();
+                    expect(addresses.length).toBeGreaterThan(0);
+                });
         });
+
+        it('should find return a list of groups', async () => {
+            let groups = [];
+            address.prototype.getAllGroups()
+                .then(res => {
+                    groups = res.data
+                })
+                .then(() => {
+                    expect(groups).not.toBeUndefined();
+                    expect(groups.length).toBeGreaterThan(0);
+                    expect(groups).toContain('Birthdays');
+                });
+                
+        })
         
     })
 })
